3e054557a8fbe0be0fd82e70a8d49617e9269567
acsokazo <csoka.zoltan@gmail.com>
2025-01-22 21:24:44 +0100

    firmware-20250122-212035

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1661,7 +1661,7 @@
 #define XY_PROBE_FEEDRATE    (133*60) // (mm/min)
 
 // Feedrate for the first approach when double-probing (MULTIPLE_PROBING == 2)
-#define Z_PROBE_FEEDRATE_FAST  (20*60) // (mm/min)
+#define Z_PROBE_FEEDRATE_FAST  (4*60) // (mm/min)
 
 // Feedrate for the "accurate" probe of each point
 #define Z_PROBE_FEEDRATE_SLOW (Z_PROBE_FEEDRATE_FAST / 2) // (mm/min)
